f6481e33786291b525fd5721322684cca93414c8
mcmk21348 <merzocid12348@gmail.com>
2020-05-07 13:36:45 +0600

    Homework

---
+++ Student!.py
@@ -0,0 +1,17 @@
+class Student:
+    def init(self, name, year, major):
+        self.name = name
+        self.major = major
+        self.year = year
+
+    def str(self):
+        return f'name: {self.name}, age: {self.year}, major: {self.major}'
+    
+
+Steve = Student('Steve Schulz', 23, 'English')
+Johny = Student('Jonathan Rosenberg', 24, 'Biology')
+Penny = Student('Penelope Meramveliotakis', 21, 'Physics')
+
+print(Steve)
+print(Johny)
+print(Penny)
